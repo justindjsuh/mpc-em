ac2a1246362d466bbc9b2e5d83b5dca3ad26650a
Justin Suh <justindjsuh@gmail.com>
2025-03-20 16:03:26 -0400

    ministries section

---
+++ src/app/ui/Landing/Ministries/Ministries.tsx
@@ -0,0 +1,15 @@
+import styles from './Ministries.module.css';
+
+const Ministries: React.FC = () => {
+  return (
+    <div className={styles.ministriesContainer}>
+      <div className={styles.ministryHeader}>
+        <p>CONNECT</p>
+        <h3>Ministries</h3>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Ministries;
