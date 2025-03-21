9618b224d24a4cdf16c49c1338621ea4532e4b49
Justin Suh <justindjsuh@gmail.com>
2025-03-20 23:03:09 -0400

    fixing loadtime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Head from 'next/head';
 import styles from './page.module.css';
 import FeaturedEvents from './ui/Landing/FeaturedEvents/FeaturedEvents';
 import HeroView from './ui/Landing/HeroView';
@@ -8,18 +9,28 @@ import Layout from './ui/Layout/Layout';
 
 export default function Home() {
   return (
-    <Layout title="">
-      <HeroView />
-      <IntroDetails />
-      <div className={styles.transitionBar}>
-        <p>Welcome to MPC EM!</p>
-        <p className={styles.desktopDetails}>•</p>
-        <p className={styles.desktopDetails}>860-930-6215</p>
-        <p className={styles.desktopDetails}>•</p>
-        <p className={styles.desktopDetails}>3260 Morris Rd, Lansdale, PA 19446</p>
-      </div>
-      <Ministries />
-      <FeaturedEvents />
-    </Layout>
+    <>
+      <Head>
+        <link
+          rel="preload"
+          as="image"
+          href="/landing_church.webp"
+          type="image/webp"
+        />
+      </Head>
+      <Layout title="">
+        <HeroView />
+        <IntroDetails />
+        <div className={styles.transitionBar}>
+          <p>Welcome to MPC EM!</p>
+          <p className={styles.desktopDetails}>•</p>
+          <p className={styles.desktopDetails}>860-930-6215</p>
+          <p className={styles.desktopDetails}>•</p>
+          <p className={styles.desktopDetails}>3260 Morris Rd, Lansdale, PA 19446</p>
+        </div>
+        <Ministries />
+        <FeaturedEvents />
+      </Layout>
+    </>
   );
 }
