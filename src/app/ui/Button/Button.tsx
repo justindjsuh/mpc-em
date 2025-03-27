b12fa831a2ef8b4306efade37e7be6e059199821
Justin Suh <justindjsuh@gmail.com>
2025-03-26 22:30:58 -0400

    button updates

--- src/app/ui/Button/Button.tsx
+++ src/app/ui/Button/Button.tsx
@@ -9,6 +9,7 @@ interface IButtonProps {
   iconTheme: 'light' | 'dark';
   filled?: boolean;
   isDisabled?: boolean;
+  showIcon?: boolean;
 };
 
 const Button: React.FunctionComponent<IButtonProps> = ({
@@ -18,23 +19,29 @@ const Button: React.FunctionComponent<IButtonProps> = ({
   iconTheme,
   filled = false,
   isDisabled = false,
+  showIcon = true,
 }) => {
   return (
     <div className={clsx(styles.buttonWrapper, {
       [styles.dark!]: theme === 'dark',
-      [styles.filled!]: filled,
+      [styles.lightFilled!]: filled && theme === 'light',
+      [styles.darkFilled!]: filled && theme === 'dark',
       [styles.hoverAnimation!]: !isDisabled,
       // [styles.disabledBtn!]: isDisabled,
     })}
     >
       <button type="button" onClick={onClick} disabled={isDisabled}>{text}</button>
-      <Image
-        aria-hidden
-        src={iconTheme === 'dark' ? '/arrow-up-right-dark.svg' : '/arrow-up-right-light.svg'}
-        alt="Arrow Facing Top Right"
-        width={22}
-        height={22}
-      />
+      {showIcon
+        ? (
+            <Image
+              aria-hidden
+              src={iconTheme === 'dark' ? '/arrow-up-right-dark.svg' : '/arrow-up-right-light.svg'}
+              alt="Arrow Facing Top Right"
+              width={21}
+              height={21}
+            />
+          )
+        : null}
     </div>
   );
 };
